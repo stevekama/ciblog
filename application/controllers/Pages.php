fc0add5cc8254be7b03c436038f2dcf86b343e13
stevekama <stevekamahertz@gmail.com>
2019-11-25 15:32:40 +0300

    bring in bootstrap

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,7 +1,7 @@
 <?php
 class Pages extends CI_Controller
 {
-    public function view($page = ""){
+    public function view($page = 'home'){
         if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
             show_404();
         }
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
     <title>Blog</title>
 </head>
 <body>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+            </div>
+            <div id="navbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="<?php echo base_url(); ?>">Home</a></li>
+                    <li><a href="<?php echo base_url(); ?>about">About</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Categories</a></li>
+                </ul>
+            
+            </div>
+        </div>
+    </nav>
+    <div class="container">
