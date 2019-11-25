e1a42966cb57d7636d5b24dc6b1eaa822ea2b36a
stevekama <stevekamahertz@gmail.com>
2019-11-25 13:20:03 +0300

    setup codeignitter

---
+++ application/views/hello.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hello World</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter!</h1>
+
+	<div id="body">
+		<p>Hello World</p>
+	</div>
+</div>
+
+</body>
+</html>
