0c4b1b44a60ec9b41dfc8bf0a1c46d0dd4d44e27
stevekama <stevekamahertz@gmail.com>
2019-11-25 17:01:05 +0300

    view post

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -3,5 +3,7 @@
     <h3><?php echo $post['title']; ?></h3>
     <small class="post-date"><?php echo $post['created_at']; ?></small><br>
     <p><?php echo $post['body']; ?></p>
+    <br><br>
+    <p><a class="btn btn-default" href="<?php echo site_url('/posts/'.$post['slug']) ?>">Read More..</a></p>
 <?php endforeach ?>  
 </ul>
